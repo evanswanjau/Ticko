2723d3ac95e405a8cd4cd8f2ee7538c3105c996e
evanswanjau <root@localhost.localdomain>
2017-01-19 14:09:51 +0300

    No databse yet simple prototype

---
+++ app.py
@@ -0,0 +1,16 @@
+#Ticket Booking Service using python command line
+#Author: Evans Wanjau
+#################################################
+
+print('######################################\n Welcome to Ticko')
+
+name = input('Please Enter your name: ')
+location = input('Hi ' + name + ' where would you like to go today? ')
+
+seat_number = '40'
+price = 350
+
+print('Name: ' + name)
+print('Heeaded to: ' + location)
+print('Seat number: ' + seat_number)
+print('Ticket price: ' , price)
